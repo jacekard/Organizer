db7e1c179a54a52aeb3d48261b1a71d9f819d0a9
Jacek Ardanowski <jacekard@gmail.com>
2018-11-19 09:16:41 +0100

    Added borderless and draggable main window

--- Organizer/Organizer/MainWindow.xaml.cs
+++ Organizer/Organizer/MainWindow.xaml.cs
@@ -20,9 +20,29 @@ namespace Organizer
     /// </summary>
     public partial class MainWindow : Window
     {
+        private readonly int WindowMargin = 100;
+
         public MainWindow()
         {
             InitializeComponent();
+
+            SizeToContent = System.Windows.SizeToContent.Manual;
         }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            this.Width = 150;
+            this.Height = SystemParameters.WorkArea.Height - this.WindowMargin;
+            this.Left = SystemParameters.WorkArea.Width - this.Width;
+            this.Top = this.WindowMargin / 2;
+        }
+
+        private void DraggableWindow(object sender, MouseButtonEventArgs e)
+
+        {
+            this.DragMove();
+        }
+
+   
     }
 }
